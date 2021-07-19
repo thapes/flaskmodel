d1532e220029d8ed0a8acbfbbb36edaa265854fb
thapes <stuczynski.thales@gmail.com>
2021-07-18 22:16:43 -0300

    Update

--- request.py
+++ request.py
@@ -2,5 +2,11 @@ import requests
 
 url = 'http://localhost:8080/api'
 
-r = requests.post(url,json={'exp':1.8,})
+# Change the value of experience that you want to test
+payload = {
+	'exp':1.8
+}
+
+r = requests.post(url,json=payload)
+
 print(r.json())
--- server.py
+++ server.py
@@ -1,20 +1,35 @@
+# Create API of ML model using flask
+
+'''
+This code takes the JSON data while POST request an performs the prediction using loaded model and returns
+the results in JSON format.
+'''
+
+# Import libraries
 import numpy as np
 from flask import Flask, request, jsonify
 import pickle
 
-
 app = Flask(__name__)
+
 # Load the model
 model = pickle.load(open('model.pkl','rb'))
 
-@app.route('/api',methods=['POST'])
+@app.route('/api/',methods=['POST'])
 def predict():
-    # Get the data from the POST request and Make prediction using model loaded from disk as per the data.
+    # Get the data from the POST request.
     data = request.get_json(force=True)
+
+    # Make prediction using model loaded from disk as per the data.
     prediction = model.predict([[np.array(data['exp'])]])
-    output = prediction[0] # Take the first value of prediction
+
+    # Take the first value of prediction
+    output = prediction[0]
 
     return jsonify(output)
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    try:
+        app.run(port=5000, debug=True)
+    except:
+        print("Server is exited unexpectedly. Please contact server admin.")
